0e82f312123cba1dcf776fe1d95c50679d80bbe1
cycleke <cycleke@hotmail.com>
2017-01-02 14:15:26 +0800

    修改：     bzoj/3506.cpp 	修改：     poj/1743.cpp 	修改：     poj/2406.cpp

--- poj/1743.cpp
+++ poj/1743.cpp
@@ -1,59 +1,62 @@
 #include <cstdio>
-#include <cstring>
-#define N 100000
-#define cmax(a,b) ((a<(__t=(b)))?a=__t:1)
-#define cmin(a,b) (((__t=(b))<a)?a=__t:1)
-
-int a[N],wa[N],wb[N],wv[N],wn[N],sa[N],rank[N],h[N],__t;
-int cmp(int *r,int a,int b,int l) {
-	return r[a]==r[b]&&r[a+l]==r[b+l];
+inline int gi() {
+	static int a; static char c;
+	while(c=getchar(),c<'0');a=c-'0';
+	while(c=getchar(),'-'<c)a=(a<<3)+(a<<1)+c-'0';
+	return a;
 }
-void DA(int *r,int n,int m) {
-	int i,j,k,p,*x=wa,*y=wb,*t;
-	for(i=0;i<m;++i) wn[i]=0;
-	for(i=0;i<n;++i) ++wn[x[i]=r[i]];
-	for(i=1;i<m;++i) wn[i]+=wn[i-1];
-	for(i=n-1;~i;--i) sa[--wn[x[i]]]=i;
-	for(j=1,p=1;p<n;j<<=1,m=p) {
-		for(p=0,i=n-j;i<n;++i) y[p++]=i;
-		for(i=0;i<n;++i)if(sa[i]>=j) y[p++]=sa[i]-j;
-		for(i=0;i<n;++i) wv[i]=x[y[i]];
-		for(i=0;i<m;++i) wn[i]=0;
-		for(i=0;i<n;++i) ++wn[wv[i]];
-		for(i=1;i<m;++i) wn[i]+=wn[i-1];
-		for(i=n-1;~i;--i) sa[--wn[wv[i]]]=y[i];
-		t=x,x=y,y=t;p=1;
-		for(x[sa[0]]=0,i=1;i<n;++i)
-		  x[sa[i]]=cmp(y,sa[i-1],sa[i],j)?p-1:p++;
+typedef int array[100000];
+array a,wa,wb,sa,rk,hi,wv,ws;
+inline bool cmp(int*r,int i,int j,int l) {
+	return r[i]==r[j]&&r[i+l]==r[j+l];
+}
+inline void da(int n,int m) {
+	static int i,j,k,p,*x=wa,*y=wb,*t;
+	for(i=0;i<m;++i)ws[i]=0;
+	for(i=0;i<n;++i)++ws[x[i]=a[i]];
+	for(i=1;i<m;++i)ws[i]+=ws[i-1];
+	for(i=n-1;~i;--i)sa[--ws[x[i]]]=i;
+	for(j=p=1;p<n;j<<=1,m=p) {
+		for(p=0,i=n-j;i<n;++i)y[p++]=i;
+		for(i=0;i<n;++i)if(sa[i]>=j)y[p++]=sa[i]-j;
+		for(i=0;i<m;++i)ws[i]=0;
+		for(i=0;i<n;++i)++ws[wv[i]=x[y[i]]];
+		for(i=1;i<m;++i)ws[i]+=ws[i-1];
+		for(i=n-1;~i;--i)sa[--ws[wv[i]]]=y[i];
+		for(t=x,x=y,y=t,p=i=1,x[sa[0]]=0;i<n;++i)
+			x[sa[i]]=cmp(y,sa[i-1],sa[i],j)?p-1:p++;
 	}
 }
-void calheight(int *r,int n) {
-	int i,j,k=0;
-	for(i=1;i<=n;++i) rank[sa[i]]=i;
-	for(i=0;i<n;h[rank[i++]]=k)
-	  for(k?--k:1,j=sa[rank[i]-1];r[i+k]==r[j+k];++k);
+inline void calheight(int n) {
+	static int i,j,k;
+	for(i=1;i<=n;++i)rk[sa[i]]=i;
+	for(i=k=0;i<n;hi[rk[i++]]=k)
+		for(k?--k:0,j=sa[rk[i]-1];a[i+k]==a[j+k];++k);
 }
-int main(){
-	int i,j,l,r,ans,mid,n,mx,mi; bool flag;
-	while(scanf("%d",&n)!=EOF&&n) {
-		for(i=0;i<n;++i) scanf("%d",a+i);
-		for(i=0,--n;i<n;++i) a[i]=a[i]-a[i+1]+100;
+int main() {
+	int i,l,r,m,n,ans,mx,mi; bool flag;
+	while(n=gi()) {
+		for(i=0;i<n;++i)a[i]=gi();
+		for(i=0,--n;i<n;++i)a[i]=a[i]-a[i+1]+100;
 		a[n]=0;
-		DA(a,n+1,200);
-		calheight(a,n); ans=0;
-		for(l=0,r=n/2;l<=r;) {
-			mid=l+r>>1;
-			flag=false;mx=mi=sa[1];
-			for(i=2;i<=n&&!flag;++i)
-			  if(h[i]<mid) mi=mx=sa[i];
-			  else {
-				  cmin(mi,sa[i]);
-				  cmax(mx,sa[i]);
-				  if(mx-mi>=mid) flag=true;
-			  }
-			flag?(l=(ans=mid)+1):r=mid-1;
+		da(n+1,200);
+		calheight(n);
+		ans=0;
+		for(l=0,r=n>>1;l<=r;) {
+			m=l+r>>1;
+			flag=false;
+			mx=mi=sa[1];
+			for(i=1;i<=n;++i)
+				if(hi[i]<m)mx=mi=sa[i];
+				else {
+					if(sa[i]>mx)mx=sa[i];
+					if(sa[i]<mi)mi=sa[i];
+					if(mx-mi>=m){flag=true;break;}
+				}
+			flag?(l=(ans=m)+1):(r=m-1);
 		}
-		if(n<9||ans<4) ans=0; else ++ans;
+		if(n<9||ans<4)ans=0;else ++ans;
 		printf("%d\n",ans);
 	}
+	return 0;
 }
--- poj/2406.cpp
+++ poj/2406.cpp
@@ -1,21 +1,16 @@
 #include <cstdio>
-#include <cstring>
-
-int nxt[4000010];
-char s[4000010];
-
+int nxt[1000000];
+char s[1000000];
 int main() {
-	while (scanf("%s", s + 1) ^ EOF) {
-		if (s[1]=='.') break;
-		int j = 0, len = 1;
-		for (int i = 2; s[i]; ++i, ++len) {
-			while (j && s[j+1] != s[i])
-			  j = nxt[j];
-			if (s[j+1]==s[i]) ++j;
-			nxt[i] = j;
+	register int i,j,len;
+	while(scanf("%s",s+1)^EOF) {
+		if(s[1]=='.') break;
+		for(i=2,j=0,len=1;s[i];++i,++len) {
+			while(j&&(s[j+1]^s[i]))j=nxt[j];
+			(s[j+1]^s[i])?0:++j;
+			nxt[i]=j;
 		}
-		if (len % (len - nxt[len])) puts("1");
-		else printf("%d\n", len / (len - nxt[len]));
-	}
-	return 0;
+		if(len%(len-nxt[len]))puts("1");
+		else printf("%d\n",len/(len-nxt[len]));
+	}return 0;
 }
